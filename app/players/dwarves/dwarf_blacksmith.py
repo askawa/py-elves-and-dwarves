aa04ef255959b7cab7e2f486c7cde9fcf93e376f
askawa <ask_awa@icloud.com>
2026-06-12 19:29:10 +0300

    annotation, strings

--- app/players/dwarves/dwarf_blacksmith.py
+++ app/players/dwarves/dwarf_blacksmith.py
@@ -9,7 +9,8 @@ class DwarfBlacksmith(Dwarf):
         self._skill_level = skill_level
 
     def player_info(self) -> str:
-        return f"Dwarf blacksmith {self.nickname} with skill of the {self._skill_level} level"
+        skill_msg = f"Dwarf blacksmith {self.nickname}"
+        return f"{skill_msg} with skill of the {self._skill_level} level"
 
     def get_rating(self) -> int:
         return self._skill_level
